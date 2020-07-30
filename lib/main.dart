ff35649f4ee1441613a2f66b1b83bf79f3c32653
Eric Lekwa <lekwaeric@yahoo.com>
2020-07-30 15:06:24 +0100

    Code Refactor
    code refactor
    
    Co-Authored-By: Eric Lekwa <lekwaeric@yahoo.com>

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
-
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -24,7 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-
   final String title;
 
   @override
@@ -32,23 +29,65 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  String username;
+  String passoword;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void showSnackbar(String msg, bool flag){
+    _scaffoldKey.currentState.showSnackBar(
+      SnackBar(
+        content: Text(msg),
+        backgroundColor: (flag) ? Colors.green : Colors.red,
+        action: SnackBarAction(label: "", textColor: Colors.white, onPressed: (){},),
+        duration: Duration(seconds: 10),
+        )
+
+    );
   }
+  void validate(){
+    print("Details");
+     username = emailController.text;
+     passoword = passwordController.text;
+     if(username.isEmpty && passoword.isEmpty){
+      showSnackbar("Fileds cannot be empty", false);
 
+     }
+    else{
+       showSnackbar(" Login details: email: $username password: $passoword", true);
+    }
+  }
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
-
-        title: Text(widget.title),
+        title: Text("Login class"),
       ),
-      body: Container(),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Login Name!"),
+              TextField(
+          
+                textInputAction: TextInputAction.search,
+                keyboardType: TextInputType.emailAddress,
+                controller: emailController,
+
+              ),
+              TextField(
+                obscureText: true,
+                controller: passwordController,
+              ),
+              FlatButton(onPressed:validate, child: Text("Login"),)
+            ],
+          ),
+        ),
+      ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
